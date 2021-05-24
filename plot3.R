9cb2813c1fc53026950494c9e0b06c96226029e9
unknown <hsyliao@gmail.com>
2021-05-23 21:15:00 -0400

    Submission 1 for the project, all four graphs generated.

---
+++ plot3.R
@@ -0,0 +1,29 @@
+# Read the data
+data <- read.table("household_power_consumption.txt", stringsAsFactors = F, header = T, sep=";")
+
+# create a column that merge date and time data together
+FullTimeDate <- strptime(paste(data$Date, data$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+data <- cbind(data, FullTimeDate)
+
+# Change the class of the columns
+
+data$Date <- as.Date(data$Date, format="%d/%m/%Y")
+data$Time <- format(data$Time, format="%H:%M:%S")
+data$Global_active_power <- as.numeric(data$Global_active_power)
+data$Global_reactive_power <- as.numeric(data$Global_reactive_power)
+data$Voltage <- as.numeric(data$Voltage)
+data$Global_intensity <- as.numeric(data$Global_intensity)
+data$Sub_metering_1 <- as.numeric(data$Sub_metering_1)
+data$Sub_metering_2 <- as.numeric(data$Sub_metering_2)
+data$Sub_metering_3 <- as.numeric(data$Sub_metering_3)
+
+# Subset data from 2007-02-01 to 2007-02-02
+feb_data <- subset(data, Date == "2007-02-01"| Date == "2007-02-02")
+
+# add lines to plot sub_metering_1 vs FullTimeDate on a graph
+png("plot3.png", width=480, height=480)
+with(feb_data, plot(FullTimeDate,Sub_metering_1, type="l", xlab="", ylab="Energy sub metering"))
+lines(feb_data$FullTimeDate, feb_data$Sub_metering_2,type="l", col= "red")
+lines(feb_data$FullTimeDate, feb_data$Sub_metering_3,type="l", col= "blue")
+legend(c("topright"), c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty= 1, lwd=2, col = c("black", "red", "blue"))
+dev.off()
